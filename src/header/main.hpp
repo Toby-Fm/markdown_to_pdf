e9372378497aa327682c65a0c5c5a007c062a0c4
Toby-Fm <mail@toby-wichmann.de>
2024-10-03 20:48:16 +0200

    Projekt Strukt

---
+++ src/header/main.hpp
@@ -0,0 +1,14 @@
+//
+// Created by toby on 03.10.24.
+//
+
+#ifndef MAIN_HPP
+#define MAIN_HPP
+#include <iostream>
+
+void hello()
+{
+    std::cout << "Hello World!\n";
+}
+
+#endif //MAIN_HPP
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by toby on 03.10.24.
+//
+
+#include "header/main.hpp"
+
+
+int main()
+{
+    hello();
+    std::cout << "Hello World!\n";
+    return 0;
+}
